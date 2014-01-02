ceaa7ee7af4b5a0b35d76aaa47412260f5fd411b
David Williams <davew1982@gmail.com>
2014-01-02 14:16:20 -0500

    added index method and associated view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Notepad2::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'notes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
