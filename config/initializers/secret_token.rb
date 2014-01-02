91b5d11a338c945bb4996975129c7f51cadabb4f
David Williams <davew1982@gmail.com>
2014-01-02 13:05:47 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notepad2::Application.config.secret_key_base = 'd7fea73510321887bbea3fdca255bce30ddbdbd617fb1f71bae2fae13c6f548f3d5c6a5b4a4cc4e87081552c95d58d5ce1333d1be58e4a2a28bb4266a722a3d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Notepad2::Application.config.session_store :cookie_store, key: '_notepad2_session'
