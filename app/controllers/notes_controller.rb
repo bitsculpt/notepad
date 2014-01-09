f71762d03b4a4b2f083c0c89725925addb96b140
David Williams <davew1982@gmail.com>
2014-01-08 22:25:28 -0500

    Notes belong to users now, and users can only see their own notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
  # making a change to test remote repository
+ before_filter :authenticate_user!, except: [:index]
  def index
   @notes = Note.all 
 end
@@ -8,7 +9,7 @@ def new
 end
 
 def create
-  @note = Note.new(note_params)
+  @note = current_user.notes.build(note_params)
   if @note.save
     redirect_to @note, notice: 'Note was successfully created.'
   else
